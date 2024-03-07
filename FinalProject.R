cd1c740d3dd0cf4f565afdeeeb4553ff0d13c12f
John Dirgo Deweese <john.dirgo@gmail.com>
2024-03-07 15:18:43 -0800

    Day 1 complete - lots of learning about the validate library

--- FinalProject.R
+++ FinalProject.R
@@ -18,6 +18,9 @@ library(rsample)    # data splitting
 library(smotefamily)       # SMOTE & Adasyn for balancing datasets
 library(readr)
 
+# Data Validator
+library(validate)
+
 # Plotting
 library(lattice)
 library(ggplot2)
@@ -55,12 +58,53 @@ library(car)          # Companion to Applied Regression
 # Feature Selection Algorithm Package
 library(Boruta)
 
-library(corpcor)      # 
+library(corpcor)
 library(mctest)
 library(corrplot)
 library(pROC)
 library(ROCR)
-library(MASS)         # Modern Applied Statistics with S
+library(MASS)       # Modern Applied Statistics with S
 library(C50)
 library(caTools)    # For Linear regression        
 library(quantmod)
+
+# Import training data file
+sal_dataV1 <- read_csv("data/EmployeeSalary_Data.csv")
+
+# We know that some of these are categorical variables (factors) even thought they are numeric
+# but we are not going to use the col_factor option on the read_csv in the beginning
+# We have a dictionary of the factors and their values, so we first want to make sure we don't
+# have out-of-range data.  Once the data quality has been checked we can convert to a factor
+# where that is appropriate.
+
+# Set up validation rules
+#
+# Rules: 
+#
+# Check the entire dataframe for NA (!is.na() - returns true if data exists)
+# Employees (Age) should be between 18 and 100
+# Categorical values coded as numbers should be within their value range
+# AverageOvertime must be positive and shouldn't be over 40
+# YearsAtCompany must be equal to or less than TotalWorkingYears
+# YearsAtCompany must be greater than or equal to YearsInCurrentRole
+# YearsAtCompany must be greater than or equal to YearsWithCurrentManager
+rules <- validator(CheckNA = !is.na(sal_dataV1),
+                   CheckAge = in_range(sal_dataV1$Age, min = 18, max = 100),
+                   CheckEducation = in_range(sal_dataV1$Education, min = 1, max = 5),
+                   CheckJobInvolve = in_range(sal_dataV1$JobInvolvement, min = 1, max = 4),
+                   CheckJobSatis = in_range(sal_dataV1$JobSatisfaction, min = 1, max = 4),
+                   CheckPerfRating = in_range(sal_dataV1$PerformanceRating, min = 1, max = 4),
+                   CheckRelSatis = in_range(sal_dataV1$RelationshipSatisfaction, min = 1, max = 4),
+                   CheckWorkLife = in_range(sal_dataV1$WorkLifeBalance, min = 1, max = 4),
+                   CheckAvgOT = in_range(sal_dataV1$AvgOverTime, min = 0, max = 40),
+                   CheckTotWorkVsCompany = sal_dataV1$YearsAtCompany <= sal_dataV1$TotalWorkingYears,
+                   CheckYrsCompVsCurRole = sal_dataV1$YearsAtCompany >= sal_dataV1$YearsInCurrentRole,
+                   CheckYrsCompVsCurMgt = sal_dataV1$YearsAtCompany >= sal_dataV1$YearsWithCurrManager)
+
+# 'Confront' the data with rules, save results
+rule_check <- confront(sal_dataV1, rules)
+
+# What do the results say?
+
+summary(rule_check)
+
